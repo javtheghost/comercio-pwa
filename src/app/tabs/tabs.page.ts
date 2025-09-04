610f909d5ec293cf35944889138c7ccceedaccf1
CMXGuille25 <guillermojr2512@gmail.com>
2025-09-04 13:04:09 -0600

    Barra de navegación personalizada, animación de transición y mejoras visuales

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,43 +1,58 @@
 import { Component } from '@angular/core';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { IonTabs, IonIcon, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tabs',
   standalone: true,
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonRouterOutlet],
+  imports: [IonTabs, IonIcon, IonRouterOutlet],
   template: `
-    <ion-tabs>
-      <ion-router-outlet></ion-router-outlet>
-
-      <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="home" href="/tabs/home">
-          <ion-icon name="home-outline"></ion-icon>
-          <ion-label>Inicio</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="search" href="/tabs/products">
-          <ion-icon name="search-outline"></ion-icon>
-          <ion-label>Buscar</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="saved" href="/tabs/orders">
-          <ion-icon name="heart-outline"></ion-icon>
-          <ion-label>Guardados</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="cart" href="/tabs/cart">
-          <ion-icon name="cart-outline"></ion-icon>
-          <ion-label>Carrito</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="account" href="/tabs/profile">
-          <ion-icon name="person-outline"></ion-icon>
-          <ion-label>Cuenta</ion-label>
-        </ion-tab-button>
-      </ion-tab-bar>
-    </ion-tabs>
-  `
+    <div class="custom-tab-bar">
+      <button (click)="navigate('/tabs/home')" [class.active]="isActive('/tabs/home')">
+        <ion-icon name="home-outline"></ion-icon>
+        <span>Inicio</span>
+      </button>
+      <button (click)="navigate('/tabs/products')" [class.active]="isActive('/tabs/products')">
+        <ion-icon name="search-outline"></ion-icon>
+        <span>Buscar</span>
+      </button>
+      <button (click)="navigate('/tabs/orders')" [class.active]="isActive('/tabs/orders')">
+        <ion-icon name="heart-outline"></ion-icon>
+        <span>Guardados</span>
+      </button>
+      <button (click)="navigate('/tabs/cart')" [class.active]="isActive('/tabs/cart')">
+        <ion-icon name="cart-outline"></ion-icon>
+        <span>Carrito</span>
+      </button>
+      <button (click)="navigate('/tabs/profile')" [class.active]="isActive('/tabs/profile')">
+        <ion-icon name="person-outline"></ion-icon>
+        <span>Cuenta</span>
+      </button>
+    </div>
+    <ion-router-outlet></ion-router-outlet>
+  `,
+  styleUrls: ['./tabs.page.scss']
 })
 export class TabsPage {
-  constructor() {}
+  tabOrder = ['/tabs/home', '/tabs/products', '/tabs/orders', '/tabs/cart', '/tabs/profile'];
+  currentTabIndex = 0;
+
+  constructor(private router: Router, private navCtrl: NavController) {
+    this.currentTabIndex = this.tabOrder.indexOf(this.router.url);
+  }
+
+  navigate(path: string) {
+    const newIndex = this.tabOrder.indexOf(path);
+    let direction: 'forward' | 'back' = 'forward';
+    if (newIndex > -1) {
+      direction = newIndex > this.currentTabIndex ? 'forward' : 'back';
+      this.currentTabIndex = newIndex;
+    }
+    this.navCtrl.navigateRoot(path, { animationDirection: direction });
+  }
+
+  isActive(path: string): boolean {
+    return this.router.url === path;
+  }
 }
